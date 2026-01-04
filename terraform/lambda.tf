f6bda689b0c66a17181b421c580fedc6a55d30c2
SAKSHITH22 <154872012+SAKSHITH22@users.noreply.github.com>
2026-01-04 17:21:26 +0530

    Add S3 invoke permission for processor_lambda

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -15,3 +15,12 @@ resource "aws_lambda_function" "report_lambda" {
   filename      = "../lambda/report.zip"
   timeout       = 3
 }
+resource "aws_lambda_permission" "allow_s3_invoke" {
+  statement_id  = "AllowS3Invoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.processor_lambda.function_name
+  principal     = "s3.amazonaws.com"
+  source_arn    = aws_s3_bucket.input_bucket.arn
+}
+
+
