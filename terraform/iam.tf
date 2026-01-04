8736836048ec181c3c2c52a17a1cbac9bce20d0b
SAKSHITH22 <154872012+SAKSHITH22@users.noreply.github.com>
2026-01-04 17:53:58 +0530

    Update iam.tf

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -13,32 +13,15 @@ resource "aws_iam_role" "lambda_role" {
   })
 }
 
-resource "aws_iam_role_policy" "lambda_policy" {
-  name = "lambda-inline-policy"
-  role = aws_iam_role.lambda_role.id
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Effect = "Allow"
-      Action = [
-        "logs:CreateLogGroup",
-        "logs:CreateLogStream",
-        "logs:PutLogEvents"
-      ]
-      Resource = "*"
-    }]
-  })
-}
-resource "aws_iam_role_policy" "lambda_policy" {
-  name = "lambda-inline-policy"
+resource "aws_iam_role_policy" "lambda_combined_policy" {
+  name = "lambda-combined-policy"
   role = aws_iam_role.lambda_role.id
 
   policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
 
-      # CloudWatch Logs (required)
+      # CloudWatch Logs
       {
         Effect = "Allow"
         Action = [
@@ -49,25 +32,19 @@ resource "aws_iam_role_policy" "lambda_policy" {
         Resource = "*"
       },
 
-      # Read from INPUT bucket
+      # Read from input bucket
       {
         Effect = "Allow"
-        Action = [
-          "s3:GetObject"
-        ]
+        Action = ["s3:GetObject"]
         Resource = "arn:aws:s3:::event-input-bucket-12345/*"
       },
 
-      # Write to OUTPUT bucket
+      # Write to output bucket
       {
         Effect = "Allow"
-        Action = [
-          "s3:PutObject"
-        ]
+        Action = ["s3:PutObject"]
         Resource = "arn:aws:s3:::event-output-bucket-12345/*"
       }
     ]
   })
 }
-
-
