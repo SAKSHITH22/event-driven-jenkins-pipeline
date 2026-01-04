bd7119f97babd87d487029a04faae0df7324ada6
SAKSHITH22 <154872012+SAKSHITH22@users.noreply.github.com>
2026-01-04 17:21:00 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,4 +9,16 @@ resource "aws_s3_bucket" "output_bucket" {
 resource "aws_cloudwatch_log_group" "lambda_logs" {
   name              = "/aws/lambda/event-processor"
   retention_in_days = 7
+
 }
+resource "aws_s3_bucket_notification" "input_trigger" {
+  bucket = aws_s3_bucket.input_bucket.id
+
+  lambda_function {
+    lambda_function_arn = aws_lambda_function.processor_lambda.arn
+    events              = ["s3:ObjectCreated:*"]
+  }
+
+  depends_on = [aws_lambda_permission.allow_s3_invoke]
+}
+
