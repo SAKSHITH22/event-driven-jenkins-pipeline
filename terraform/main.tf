4215b13e7640ed0271c5ed75b5631734dcbb197c
SAKSHITH22 <154872012+SAKSHITH22@users.noreply.github.com>
2026-01-04 18:58:55 +0530

    Add IAM role policy for Lambda function
    Added IAM role policy for Lambda function access to CloudWatch and S3 buckets.

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,24 +1,35 @@
-resource "aws_s3_bucket" "input_bucket" {
-  bucket = "event-input-bucket-12345"
+resource "aws_iam_role_policy" "lambda_policy" {
+  name = "lambda-inline-policy"
+  role = aws_iam_role.lambda_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+
+      # CloudWatch Logs
+      {
+        Effect = "Allow"
+        Action = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ]
+        Resource = "*"
+      },
+
+      # S3 Read from input bucket
+      {
+        Effect = "Allow"
+        Action = ["s3:GetObject"]
+        Resource = "arn:aws:s3:::event-input-bucket-12345/*"
+      },
+
+      # S3 Write to output bucket
+      {
+        Effect = "Allow"
+        Action = ["s3:PutObject"]
+        Resource = "arn:aws:s3:::event-output-bucket-12345/*"
+      }
+    ]
+  })
 }
-
-resource "aws_s3_bucket" "output_bucket" {
-  bucket = "event-output-bucket-12345"
-}
-
-resource "aws_cloudwatch_log_group" "lambda_logs" {
-  name              = "/aws/lambda/event-processor"
-  retention_in_days = 7
-
-}
-resource "aws_s3_bucket_notification" "input_trigger" {
-  bucket = aws_s3_bucket.input_bucket.id
-
-  lambda_function {
-    lambda_function_arn = aws_lambda_function.processor_lambda.arn
-    events              = ["s3:ObjectCreated:*"]
-  }
-
-  depends_on = [aws_lambda_permission.allow_s3_invoke]
-}
-
