472d7abeaed3463207883fdcf2fe023b2d7a330e
SAKSHITH22 <154872012+SAKSHITH22@users.noreply.github.com>
2026-01-04 19:48:05 +0530

    s3.tf

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -6,14 +6,6 @@ resource "aws_s3_bucket" "output_bucket" {
   bucket = "event-output-bucket-12345"
 }
 
-resource "aws_lambda_permission" "allow_s3_invoke" {
-  statement_id  = "AllowS3Invoke"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.processor_lambda.function_name
-  principal     = "s3.amazonaws.com"
-  source_arn    = aws_s3_bucket.input_bucket.arn
-}
-
 resource "aws_s3_bucket_notification" "input_trigger" {
   bucket = aws_s3_bucket.input_bucket.id
 
